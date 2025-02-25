fc8db4649b8b59fd765db0f4399be92d94b58b81
phiwan-dev <st172340@stud.uni-stuttgart.de>
2025-02-25 13:55:09 +0100

    make chain history aware

--- llm.py
+++ llm.py
@@ -28,23 +28,60 @@ vector: FAISS = FAISS.from_documents(documents, embeddings)
 
 # create document chain
 from langchain.chains.combine_documents import create_stuff_documents_chain # type: ignore
-prompt = ChatPromptTemplate.from_template(
-"""Answer the following question based only on the provided context:
-<context>
-{context}
-</context>
-Question: {input}""")
-document_chain = create_stuff_documents_chain(llm, prompt)
+# prompt = ChatPromptTemplate.from_template(
+# """Answer the following question based only on the provided context:
+# <context>
+# {context}
+# </context>
+# Question: {input}""")
+# document_chain = create_stuff_documents_chain(llm, prompt)
 
-# create retrieval chain
+# # create retrieval chain
 from langchain.chains import create_retrieval_chain
 from langchain_core.vectorstores import VectorStoreRetriever
 retriever: VectorStoreRetriever = vector.as_retriever()
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
+# retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+# history aware retriever
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+prompt = ChatPromptTemplate.from_messages([
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+    ("user", "Given the above conversation, generate a search query to look up to get information relevant to the conversation")
+])
+retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+# create history aware document chain
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("user", "{input}"),
+])
+document_chain = create_stuff_documents_chain(llm, prompt)
+# history aware retrieval chain
+retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
+
+# test
+from langchain_core.messages import HumanMessage, AIMessage
+chat_history = [HumanMessage(content="Can LangSmith help test my LLM applications?"), AIMessage(content="Yes!")]
+response = retrieval_chain.invoke({
+    "chat_history": chat_history,
+    "input": "Tell me how"
+})
+print(response["answer"])
+
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+#     MessagesPlaceholder(variable_name="chat_history"),
+#     ("user", "{input}"),
+# ])
+# document_chain = create_stuff_documents_chain(llm, prompt)
+# retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
 
 # get output
-response = retrieval_chain.invoke({"input": "how can langsmith help with testing?"})
-print(response["answer"])
+#response = retrieval_chain.invoke({"input": "how can langsmith help with testing?"})
+#print(response["answer"])
 
 
 
