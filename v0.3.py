447738178b9011fe1866c5bc0faecb1e5068b072
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-03-10 12:36:18 +0100

    move to custom generation prompt

--- v0.3.py
+++ v0.3.py
@@ -22,17 +22,19 @@ from langchain_ollama import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 embeddings: OllamaEmbeddings = OllamaEmbeddings(model=MODEL_NAME)
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=200,chunk_overlap=20,length_function=len)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100,length_function=len)
 documents: List[Document] = text_splitter.split_documents(raw_documents)
 vector_store = Chroma(embedding_function=embeddings, persist_directory="./cache")
-print(f"{len(documents)}")
-for doc in documents:
-    _ = vector_store.add_documents([doc])
-    print("#", end="", flush=True)
+#print("  [raw docs]")
+#for doc in raw_documents:
+#    print(doc.page_content)
+#print(f"  [processed docs] ({len(documents)})")
+#for doc in documents:
+#    #_ = vector_store.add_documents(documents)
+#    print("#", end="", flush=True)
+#print("")
 
 
-
-from langchain import hub
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langgraph.graph import START, StateGraph
@@ -40,7 +42,14 @@ from typing_extensions import List, TypedDict
 
 
 # Define prompt for question-answering
-prompt = hub.pull("rlm/rag-prompt")
+from langchain_core.prompts import PromptTemplate
+raw_prompt = """Answer the question at the end using the given context below.
+Do not make up an answer.
+
+{context}
+
+Question: {question}"""
+generation_prompt = PromptTemplate.from_template(raw_prompt)
 
 
 # Define state for application
@@ -58,7 +67,7 @@ def retrieve(state: State):
 
 def generate(state: State):
     docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    messages = generation_prompt.invoke({"question": state["question"], "context": docs_content})
     response = llm.invoke(messages)
     return {"answer": response.content}
 
@@ -69,6 +78,7 @@ graph_builder.add_edge(START, "retrieve")
 graph = graph_builder.compile()
 
 
-
-for message, _ in graph.stream({"question": "What npc are there"}, stream_mode="messages"):
+# print output
+print("\n\n   [answer]")
+for message, metadata in graph.stream({"question": "Who is Sparragon"}, stream_mode="messages"):
     print(f"{message.content}", end="", flush=True)
