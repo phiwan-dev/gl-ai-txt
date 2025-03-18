e580a334087eebeeeb7434a4a8ce8bf13eb7895f
phiwan-dev <75113544+phiwan-dev@users.noreply.github.com>
2025-03-19 00:08:34 +0100

    update dependencies to latest version

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
     description = "Python 3.12 development environment";
 
+    inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     outputs = { self, nixpkgs }:
     let
         system = "x86_64-linux";
